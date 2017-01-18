4de0870a579896f690b132e4f83916799f7c1195
caaasssasian <cassidyctho@hotmail.com>
2017-01-18 14:49:13 -0500

    added "click me" button

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,14 @@
 /* javascript lives here */
 
 console.log("App Started");
+
+// create a reference to the button on teh page (index.html)
+var clickMeButton = document.getElementById("clickMeButton");
+
+// add an eent listener - for the click event and call the Click function
+clickMeButton.addEventListener("click", Click)
+
+// click function - used as an event handler
+function Click() {
+    console.log("clicked!");
+}
